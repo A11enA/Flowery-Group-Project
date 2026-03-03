cfe1f35bfc38ced851c84c76001ed35b2c94a5ef
midoxiden <galaxpandaxox@gmail.com>
2026-03-03 15:21:58 -0600

    sprint + npc + talking

---
+++ Scripts/npc.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+func _on_interact_body_entered(body: Node2D) -> void:
+	if body is Player:
+		$Button.visible = true
+
+func _on_interact_body_exited(body: Node2D) -> void:
+	if body is Player:
+		$Button.visible = false
+
+func _on_button_pressed() -> void:
+	Dialogic.start("Tutorial")
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 class_name Player
 
-@export var move_speed: float = 500.0
+@export var move_speed: float = 100.0
 @export var acceleration: float = 10.0
+@export var sprint = 1.5
+var direction 
 
 func _ready() -> void:
 	pass
@@ -14,4 +16,7 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = move_vector * move_speed
+
+	if Input.is_action_pressed("sprint"):
+			velocity *= sprint
 	move_and_slide()
