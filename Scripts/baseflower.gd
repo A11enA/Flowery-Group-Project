d2a1e520df655a01d922c27bb64dc50ca157c79f
midoxiden <galaxpandaxox@gmail.com>
2026-03-05 15:12:21 -0600

    flower attributes

---
+++ Scripts/baseflower.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var attributes : flower_attributes
+var value: float
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animation : AnimationPlayer = $AnimationPlayer
+@onready var sound : AudioStreamPlayer = $AudioStreamPlayer
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	value = attributes.value
+	sprite.texture = attributes.texture
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		animation.play()
