04da395aac9534a16044aef81e2ae1fcbdca6cb4
ldupclay27-ship-it <ldupclay27@bfhsla.org>
2026-03-24 14:44:44 -0500

    walking

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -15,9 +15,26 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = move_vector * move_speed
+	#var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
+	#velocity = move_vector * move_speed
+	move_player()
 
 	if Input.is_action_pressed("sprint"):
 			velocity *= sprint
 	move_and_slide()
+func move_player():
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	#velocity = move_vector * move_speed
+	velocity = velocity.move_toward(move_vector * move_speed, acceleration)
+	
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("walk_right")
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("walk_left")
+	elif velocity.y > 0:
+		$AnimatedSprite2D.play("walk_down")
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play("walk_up")
+	else:
+		$AnimatedSprite2D.stop()
+	
