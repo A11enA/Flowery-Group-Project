54aded6cf42209a832a7cd58d3f4111dbf898022
A11enA <alasiakallen@gmail.com>
2026-04-17 15:08:49 -0500

    Stacking System Working!!!!

--- Scenes/Scripts/inv_ui_slot.gd
+++ Scenes/Scripts/inv_ui_slot.gd
@@ -1,13 +1,26 @@
 extends Panel
 
 @onready var item_visual: TextureRect = $TextureRect
+@onready var stack_label: Label = $StackAmount
 
-func set_item(item):
-	if item:
+var current_entry = null # stores the full stack (item + quantity)
+
+func set_item(entry):
+	current_entry = entry
+	
+	if entry:
+		var item = entry["item"]
+		var quantity = entry["quantity"]
+		
+		# Set icon
 		item_visual.texture = item.icon
+		
+		# Handle stack text (Inventory Forge style)
+		if quantity > 1:
+			stack_label.text = str(quantity)
+			stack_label.visible = true
+		else:
+			stack_label.visible = false
 	else:
 		item_visual.texture = null
-
-
-func _on_button_pressed():
-	pass # Equip selected item
+		stack_label.visible = false
--- Scenes/Scripts/inventorymanager.gd
+++ Scenes/Scripts/inventorymanager.gd
@@ -1,6 +1,6 @@
 class_name  InventoryControl extends Node
 
-var inventory := []
+var inventory := [] # [{item: item_resource, quantity: int}]
 signal inventory_updated(inventory)
 const DATABASE_PATH := "res://addons/inventory_forge/demo/demo_database.tres"
 
@@ -8,12 +8,28 @@ const DATABASE_PATH := "res://addons/inventory_forge/demo/demo_database.tres"
 
 func _ready():
 	Global.inventory_manager = self
+	
+
+func find_stack(id):
+	for entry in inventory:
+		if entry.item.id == id:
+			return entry
+	return null
 
 func addToInventory(id) -> void:
 	var item = database.get_item_by_id(id)
-	
-	if item:
-		inventory.append(item)
-		emit_signal("inventory_updated", inventory)
-	else:
+	if not item:
 		print("Item not found:", id)
+		return
+	
+	var stack = find_stack(id)
+	
+	if stack:
+		stack.quantity += 1
+	else:
+		inventory.append({
+			"item": item,
+			"quantity": 1
+		})
+	
+	emit_signal("inventory_updated", inventory)
